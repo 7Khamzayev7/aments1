c02d93f183cbba555ec166cdc0f4dea93c234af0
Nodirbek <xamzayevnodirbek29@gmail.com>
2024-07-10 05:39:39 +0500

    corrected index, full bloh html, and views in blog app, and corrected staticfiles

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from blog.models import Author, Post
+from typing import Any
+from django.db.models.query import QuerySet
+from django.views.generic import ListView, DetailView, CreateView
+from django.db.models import Count
+from django.urls import reverse
+from django.shortcuts import get_object_or_404
+from blog.forms import CommentForm
 
-# Create your views here.
+class Blog(ListView):
+    template_name = 'blog.html'
+
